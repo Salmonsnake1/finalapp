cdf558455c40f43301b707d0aeecf29e6cde416f
Salmonsnake1 <samueldyson1@outlook.com>
2024-05-12 16:00:45 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.finalapp',
+  appName: 'finalapp',
+  webDir: 'dist'
+};
+
+export default config;
